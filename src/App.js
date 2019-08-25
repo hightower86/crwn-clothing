243728f499db31db84a813f1d143890cc942d241
Adel <htwr86@gmail.com>
2019-08-25 20:30:02 +0300

    66 withRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Homepage from './components/pages/Homepage/Homepage';
 import './App.css';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats PAGE</h1>
+  </div>
+);
 
 const App = () => {
   return (
     <div className='App'>
-      <Homepage />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/shop/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 };
