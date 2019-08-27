08061149c5e06633c488fe00cddec08a78144b02
Adel <htwr86@gmail.com>
2019-08-27 22:11:32 +0300

    70 header component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Homepage from './components/pages/Homepage/Homepage';
 import Shop from './components/pages/Shop';
+import Header from './components/Header';
+
 import './App.css';
 
 const App = () => {
   return (
     <div className='App'>
+      <Header />
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route path='/shop' component={Shop} />
---
+++ src/components/Header/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+import { Link } from 'react-router-dom';
+import './Header.scss';
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <Link to='/' className='logo-container'>
+        <Logo className='logo' />
+      </Link>
+      <div className='options'>
+        <Link to='/shop' className='option'>
+          SHOP
+        </Link>
+        <Link to='/shop' className='option'>
+          CONTACT
+        </Link>
+        <Link to='/shop' className='option'>
+          SHOP
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
