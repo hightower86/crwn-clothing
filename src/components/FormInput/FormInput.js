b1e12a24581feb66029ec73a8233e226ec93ec32
Adel <htwr86@gmail.com>
2019-08-28 19:42:51 +0300

    74.5 form-input

--- src/components/FormInput/FormInput.js
+++ src/components/FormInput/FormInput.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import './FormInput.scss';
 
-function FormInput() {
-  return <div>FormInput component</div>;
-}
+const FormInput = ({ handleChange, label, ...otherProps }) => {
+  return (
+    <div className='group'>
+      <div className='form-input' onChange={handleChange} {...otherProps}>
+        {label ? (
+          <label
+            className={`${
+              otherProps.value.length ? 'shrink' : ''
+            } form-input-label`}
+          >
+            {label}
+          </label>
+        ) : null}
+      </div>
+    </div>
+  );
+};
 
 export default FormInput;
