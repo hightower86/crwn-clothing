af44348972ce1a763d4b53cc9996940872264ac8
Adel <htwr86@gmail.com>
2019-08-29 18:59:04 +0300

    82.99 handle authentication in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { auth } from './utils/firebase/firebase';
 import Homepage from './components/pages/Homepage/Homepage';
 import Shop from './components/pages/Shop';
 import SignInSignUp from './components/pages/SignInSignUp';
@@ -7,18 +8,37 @@ import Header from './components/Header';
 
 import './App.css';
 
-const App = () => {
-  return (
-    <div className='App'>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={Homepage} />
-        <Route path='/shop' component={Shop} />
-        {/* <Route path='/contact' component={Contact} /> */}
-        <Route path='/signin' component={SignInSignUp} />
-      </Switch>
-    </div>
-  );
-};
+class App extends React.Component {
+  state = {
+    currentUser: null
+  };
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Homepage} />
+          <Route path='/shop' component={Shop} />
+          {/* <Route path='/contact' component={Contact} /> */}
+          <Route path='/signin' component={SignInSignUp} />
+        </Switch>
+      </div>
+    );
+  }
+}
 
 export default App;
