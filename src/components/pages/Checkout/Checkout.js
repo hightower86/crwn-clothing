1e5087da0062077f18da53a875e3fa6b4c77c41c
Adel <htwr86@gmail.com>
2020-05-30 16:14:57 +0300

    123. checkout table ready

--- src/components/pages/Checkout/Checkout.js
+++ src/components/pages/Checkout/Checkout.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selectCartItems } from '../../../redux/cart/cart-selectors';
 
 import styled from 'styled-components';
 
@@ -12,34 +15,64 @@ const Wrapper = styled.div`
   font-size: 1.2rem;
 `;
 
-const CheckoutHeader = styled.div`
+const ItemsRow = styled.div`
   width: 100%;
-  height: 40px;
   display: flex;
   justify-content: space-between;
+  align-items: center;
   border-bottom: 1px solid darkgrey;
 `;
 
-const HeaderBlock = styled.span`
+const CheckoutHeader = styled(ItemsRow)`
+  color: red;
+  height: 40px;
+  position: sticky;
+`;
+const CheckoutBody = styled.div`
+  color: blue;
+`;
+
+const ItemBlock = styled.span`
   text-transform: capitalize;
   width: 23%;
   &:last-child {
     width: 8%;
   }
 `;
+const Img = styled.img`
+  height: 70px;
+  margin: 5px 0;
+`;
 
-const Checkout = () => {
+const Checkout = ({ cartItems }) => {
   return (
     <Wrapper>
       <CheckoutHeader>
         {['Product', 'Description', 'Quantity', 'Price', 'Remove'].map(
           (item) => (
-            <HeaderBlock>{item}</HeaderBlock>
+            <ItemBlock>{item}</ItemBlock>
           )
         )}
       </CheckoutHeader>
+      {cartItems.map(({ id, name, price, imageUrl, quantity }) => (
+        <ItemsRow key={id}>
+          <ItemBlock>
+            <Img src={imageUrl} alt={name} />
+          </ItemBlock>
+          <ItemBlock>{name}</ItemBlock>
+          <ItemBlock>{quantity}</ItemBlock>
+          <ItemBlock>${price}</ItemBlock>
+          <ItemBlock>X</ItemBlock>
+        </ItemsRow>
+      ))}
+
+      <pre>{JSON.stringify(cartItems, null, 2)}</pre>
     </Wrapper>
   );
 };
 
-export default Checkout;
+const mapStateToProps = createStructuredSelector({
+  cartItems: selectCartItems,
+});
+
+export default connect(mapStateToProps)(Checkout);
